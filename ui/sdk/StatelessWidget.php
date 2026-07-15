fc370c7e7af54d18c48e1fd0c037b8f52b0738a8
Ronaldo <ronaldo.awademe@epitech.eu>
2026-07-15 20:40:27 +0100

    Remove abandoned Flutter transpiler (pivoting to native PHP runtime)

--- ui/sdk/StatelessWidget.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Sdk;
-
-abstract class StatelessWidget extends Widget
-{
-    abstract public function build(): Widget;
-}
--- ui/sdk/Widget.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sdk;
-
-abstract class Widget
-{
-}
