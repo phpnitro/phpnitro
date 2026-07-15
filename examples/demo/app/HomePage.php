ab3103fc886cbe6a382c403c94b62d769411b90c
Ronaldo <ronaldo.awademe@epitech.eu>
2026-07-15 20:04:31 +0100

    Showcase Env::get() with .env in the demo example

--- examples/demo/app/HomePage.php
+++ examples/demo/app/HomePage.php
@@ -7,13 +7,14 @@ use Sdk\Text;
 use Sdk\Button;
 use Sdk\Container;
 use Sdk\SizedBox;
+use Sdk\Env;
 
 class HomePage extends StatelessWidget
 {
     public function build(): Widget
     {
         return Column::new([
-            Container::new(Text::new('Mon application'), color: '#2196F3'),
+            Container::new(Text::new(Env::get('APP_NAME')), color: '#2196F3'),
             SizedBox::new(height: 16.0),
             Button::new('Connexion'),
         ]);
